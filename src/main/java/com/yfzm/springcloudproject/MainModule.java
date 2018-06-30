e8f6fcf5bb8d91d09b41396ec3060e8d659c4828
Li Hao <yfzm1223@sjtu.edu.cn>
2018-06-30 06:05:03 +0000

    prepare for docker

---
+++ src/main/java/com/yfzm/springcloudproject/MainModule.java
@@ -0,0 +1,12 @@
+package com.yfzm.springcloudproject;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainModule {
+    public static void main(String[] args) {
+        SpringApplication.run(MainModule.class, args);
+    }
+}
