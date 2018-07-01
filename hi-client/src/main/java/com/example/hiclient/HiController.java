1aacab3d0f0c08632b420a92bddf965c097cd486
WangPuyue <wangpy1489@sjtu.edu.cn>
2018-07-01 10:57:35 +0800

    集成rabbitmq 支持消息代理刷新配置

--- hi-client/src/main/java/com/example/hiclient/HiController.java
+++ hi-client/src/main/java/com/example/hiclient/HiController.java
@@ -2,13 +2,15 @@ package com.example.hiclient;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HiController {
 
-    @Value("${server.port}")
+    @Value("${foo}")
     private String port;
 
     @GetMapping("/hi")
